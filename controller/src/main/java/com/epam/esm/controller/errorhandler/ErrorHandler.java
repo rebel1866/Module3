66cdca66f388190ce5d9f213385ee781f0c4dcd7
Stanislav <rebel1866@yandex.ru>
2022-04-03 19:38:37 +0300

    error handler is updated.

--- controller/src/main/java/com/epam/esm/controller/errorhandler/ErrorHandler.java
+++ controller/src/main/java/com/epam/esm/controller/errorhandler/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.epam.esm.controller.errorhandler;
 
 
 import com.epam.esm.controller.exceptions.RestControllerException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -10,19 +11,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
-    private final static String responseMessage = """
-            {
-            "errorMessage":"%s",
-            "error code":"%s",
-            "HTTP status":"%s",
-            "Cause:":"%s",
-            }""";
     private final static Map<String, HttpStatus> codesAndStatuses = new HashMap<>();
 
     static {
@@ -47,8 +42,61 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
         } else {
             causeMessage = new StringBuilder(resException.getMessage());
         }
-        String response = String.format(responseMessage, resException.getMessage(), resException.getErrorCode(),
-                status, causeMessage);
+        ErrorMessage errorMessage = new ErrorMessage(resException.getMessage(), resException.getErrorCode(),
+                status, causeMessage.toString());
+        String response = "Error has occurred";
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            response = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(errorMessage);
+        } catch (IOException e) {
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return new ResponseEntity<>(response, status);
+    }
+
+    static class ErrorMessage {
+        private String errorMessage;
+        private String errorCode;
+        private HttpStatus httpStatus;
+        private String cause;
+
+        public ErrorMessage(String errorMessage, String errorCode, HttpStatus httpStatus, String cause) {
+            this.errorMessage = errorMessage;
+            this.errorCode = errorCode;
+            this.httpStatus = httpStatus;
+            this.cause = cause;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+        public void setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorCode() {
+            return errorCode;
+        }
+
+        public void setErrorCode(String errorCode) {
+            this.errorCode = errorCode;
+        }
+
+        public HttpStatus getHttpStatus() {
+            return httpStatus;
+        }
+
+        public void setHttpStatus(HttpStatus httpStatus) {
+            this.httpStatus = httpStatus;
+        }
+
+        public String getCause() {
+            return cause;
+        }
+
+        public void setCause(String cause) {
+            this.cause = cause;
+        }
     }
 }
